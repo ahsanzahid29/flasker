c3e7cf1a3090859243522599a0c40be2a7c99833
ahsan zahid <ahsanzahid29@hotmail.com>
2022-06-20 15:56:48 +0500

    Comment added

--- hello.py
+++ hello.py
@@ -19,7 +19,7 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"),404
-
+#internal server error 
 @app.errorhandler(500)
 def server_not_found(e):
     return render_template("500.html"),500
