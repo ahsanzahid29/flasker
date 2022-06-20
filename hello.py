6e7e4ce0daf14ed6e8950c07b6215d70a0deabd2
ahsan zahid <ahsanzahid29@hotmail.com>
2022-06-20 15:47:28 +0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/hello')
+def hello():
+    return 'Hello, Ahsan'
+
+@app.route('/user/<name>')
+def user(name):
+    stuff='I am <strong>PHP Developer</strong>'
+    #return '<h1>Hello {}</h1>'.format(name)
+    return render_template("index.html" , name=name,stuff=stuff)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+@app.errorhandler(500)
+def server_not_found(e):
+    return render_template("500.html"),500
